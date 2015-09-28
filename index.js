c324b2dc171a329808a5ed16f119cde76bac528a
Judd Flamm <kd7yva@gmail.com>
2015-09-28 12:00:39 -0600

    added redis

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
+var redisConnectionString = "redis://redistogo:8f4f25e5043da7a2e53deb0a5f149824@bluegill.redistogo.com:9504/";
+var redis = require("redis").createClient(redisConnectionString);
+redis.on("error", function (err) {
+    console.log("Error " + err);
+});
+
 var express = require('express');
 var app = express();
 
-var webhookCalledCount = 0;
+//var webhookCalledCount = 0;
 
 app.get('/', function (req, res) {
-  res.send('webhook called: ' + webhookCalledCount + ' times');
+  client.get('myCounter', function(err, reply) {
+    res.send('webhook called: ' + reply + ' times');
+  });
 });
 
 app.get('/webhook', function (req, res) {
-  webhookCalledCount++;
-  res.send('success');
+  //webhookCalledCount++;
+  redis.incr('myCounter', function(err, reply) {
+    res.send('myCounter: ' + reply);
+  });
 });
 
 var server = app.listen((process.env.PORT || 5000), function () {
